876d736fa149757552cc7bbd60ebef60e9890b3a
Oscar Povea <oscaarpoveaa@gmail.com>
2023-01-23 12:01:26 +0100

    Sigo

--- Herramientas/Pasteleria.php
+++ Herramientas/Pasteleria.php
@@ -3,7 +3,59 @@
 <?php
 class Pasteleria
 {
-    function __construct(private string $nombre, private $productos = array(), private int $numProductos, private $clientes = array(), private int $numClientes,)
+    private $productos = array();
+    private $clientes = array();
+    private int $numProductos;
+    function __construct(private string $nombre, private int $numClientes)
     {
     }
-}
+
+    private function incluirProducto(Dulce $producto){
+        array_push($this->productos, $producto);
+    }
+    
+    public function incluirTarta($nombre,$precio,$numPisos,$rellenos,$minC,$maxC){
+        $tarta = new Tarta($nombre, $this->getNumProductos(), $precio, $rellenos, $numPisos, $maxC, $minC);
+        $this->incluirProducto($tarta);
+    }
+    public function incluirBollo($nombre,$precio,$relleno){
+        $bollo = new Bollo($nombre,$this->getNumProductos(),$precio,$relleno);
+        $this->incluirProducto($bollo);
+    }
+    public function incluirChocolate($nombre,$precio,$portentajeCacao,$peso){
+        $chocolate = new Chocolate($nombre,$this->getNumProductos(),$precio,$portentajeCacao,$peso);
+        $this->incluirProducto($chocolate);
+    }
+    public function incluirCliente($nombre, $numero, $numDulcesComprados, $numPedidosEfectuados){
+        $cliente = new Cliente($nombre,$numero, $numDulcesComprados, $numPedidosEfectuados);
+        array_push($this->clientes, $cliente);
+
+    }
+    public function listarProductos(){
+        {echo '<br> ---------------------------------------------------------------------------- <br>';
+            echo 'Lista de productos :';
+            for ($i = 0; $i < count($this->productos); $i++) {
+                echo '<br>' . $this->productos[$i]->nombre;
+            }
+            echo '<br> ----------------------------------------------------------------------------';
+        }
+    }
+    public function listarClientes()
+    { {
+            echo '<br> ---------------------------------------------------------------------------- <br>';
+            echo 'Lista de clientes :';
+            for ($i = 0; $i < count($this->clientes); $i++) {
+                echo '<br>' . $this->clientes[$i]->nombre;
+            }
+            echo '<br> ----------------------------------------------------------------------------';
+        }
+    }
+    public function comprarClienteProducto($numeroCliente,$numeroDulce){
+
+    }
+    
+    public function getNumProductos()
+    {
+        return count($this->productos);
+    }
+}   
